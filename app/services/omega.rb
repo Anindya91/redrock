8fd51b0b96e16a5dcc256f9a09be9a12a8c8c795
Anindya Mondal <>
2022-05-24 17:27:34 -0400

    active accounts query

--- app/services/omega.rb
+++ app/services/omega.rb
@@ -1,5 +1,6 @@
 class Omega
-  BATCH_SIZE = 50000
+  ACTIVE_CODES = %w(A AV)
+
   def initialize
     @client = TinyTds::Client.new(
       username: Rails.application.credentials.omega_username,
@@ -9,18 +10,28 @@ class Omega
     )
   end
 
-  def get_accounts
-    data = []
-    query_string = "SELECT TOP(#{data.count + BATCH_SIZE}) * FROM Common.Account"
-    result = @client.execute(query_string)
-    result.each { |r| data << r }
+  def get_active_accounts
+    result = @client.execute("SELECT Id FROM Common.AdminStatus WHERE #{query_string("Code", ACTIVE_CODES)}")
+    status_ids = result.to_a.map { |s| s["Id"] }
 
-    close_connection and return data
+    query_string = "SELECT * FROM Common.Account WHERE #{query_string("AdminStatusId", status_ids)}"
+    result = @client.execute(query_string)
+
+    close_connection and return result.to_a
   end
 
   private
 
   def close_connection
     @client.close
+  end
+
+  def query_string(key, values)
+    arr = []
+    values.each do |v|
+      arr << "#{key} = '#{v}'"
+    end
+
+    arr.join(" OR ")
   end
 end
