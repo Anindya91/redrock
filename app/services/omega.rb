bd32f02c868cc6219faa07e93f5e88f82c1c3196
Anindya Mondal <>
2022-05-24 16:59:21 -0400

    Omega client query updates

--- app/services/omega.rb
+++ app/services/omega.rb
@@ -1,6 +1,5 @@
 class Omega
-  BATCH_SIZE = 1000
-
+  BATCH_SIZE = 50000
   def initialize
     @client = TinyTds::Client.new(
       username: Rails.application.credentials.omega_username,
@@ -12,23 +11,9 @@ class Omega
 
   def get_accounts
     data = []
-    ids = []
-
-    100.times do |i|
-      query_string = "SELECT TOP(#{ids.count + BATCH_SIZE}) * FROM Common.Account"
-      if ids.present?
-        query_string = "#{query_string} EXCEPT SELECT TOP(#{ids.count}) * FROM Common.Account"
-      end
-
-      result = @client.execute(query_string)
-      new_ids = []
-      result.each do |r|
-        data << r
-        new_ids << r["Id"]
-      end
-      break if new_ids.blank?
-      ids += new_ids
-    end
+    query_string = "SELECT TOP(#{data.count + BATCH_SIZE}) * FROM Common.Account"
+    result = @client.execute(query_string)
+    result.each { |r| data << r }
 
     close_connection and return data
   end
